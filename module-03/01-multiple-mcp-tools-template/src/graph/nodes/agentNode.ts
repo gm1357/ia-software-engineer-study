b94506fecb88e4fba0a5521f8d04dfde6dd56fcd
Gabriel <g_machado1@hotmail.com>
2026-06-02 15:31:21 -0300

    feat(module-03/01): implement mongodb mcp tool

--- module-03/01-multiple-mcp-tools-template/src/graph/nodes/agentNode.ts
+++ module-03/01-multiple-mcp-tools-template/src/graph/nodes/agentNode.ts
@@ -1,13 +1,24 @@
 import { AIMessage } from "langchain";
 import { OpenRouterService } from "../../services/openRouterService.ts";
 import type { GraphState } from "../state.ts";
+import { getSystemPrompt, getUserPrompt } from "../../prompts/v1/agentNode.ts";
 
 export function agentNode(openRouterService: OpenRouterService) {
   return async (state: GraphState): Promise<Partial<GraphState>> => {
     console.log("🤖 Agent node processing...");
     try {
+      const userMessage = getUserPrompt({
+        intent: state.intent!,
+        fileName: state.fileName!,
+        fileContent: state.fileContent!,
+      });
+      const result = await openRouterService.generateStructured(
+        getSystemPrompt(),
+        userMessage,
+      );
+
       return {
-        messages: [new AIMessage("Nothing yet!")],
+        messages: [new AIMessage(result.data as string)],
       };
     } catch (error) {
       console.error("Agent error:", error);
--- module-03/01-multiple-mcp-tools-template/src/services/mcpService.ts
+++ module-03/01-multiple-mcp-tools-template/src/services/mcpService.ts
@@ -1,3 +1,17 @@
+import { MultiServerMCPClient } from "@langchain/mcp-adapters";
+import { getMongoDbTool } from "../tools/mongodbTool.ts";
+
 export const getMCPTools = async () => {
-  return [];
+  const client = new MultiServerMCPClient({
+    mcpServers: {
+      ...getMongoDbTool(),
+    },
+    onMessage: (log, source) => {
+      console.log(`[${source.server}] ${log.data}`);
+    },
+  });
+
+  const mcpTools = await client.getTools();
+
+  return [...mcpTools];
 };
